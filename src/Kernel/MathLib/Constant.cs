6ab5e7966595efaadbff8330b9452428c92de2c4
Zq-est <ZqMyr206088@outloook.com>
2026-08-15 10:17:50 +0800

    (feat) -Add Constant struce with common mathematical constants -Introduce a new Kernel.MathLib.Constant struct containing frequently used constants such as Pi, E, and epsilon values.

---
+++ src/Kernel/MathLib/Constant.cs
@@ -0,0 +1,20 @@
+﻿namespace Kernel.MathLib;
+
+public struct Constant
+{
+    public const double Epsilon = 1E-06;
+    public const double EpsilonRadians = Math.PI / 180;
+    
+    public const double Pi = 3.14159265358979323846;
+    public const double E = 2.71828182845904523536;
+    public const double Tau = 6.2831853071795864769252867666;
+
+    public const double Sqrt2 = 1.4142135623730950488016887242;
+    public const double Sqrt3 =1.7320508075688772935274463415059;
+    public const double Sqrt12 = 0.7071067811865475244008443621048490;
+    public const double Sqrt13 = 0.57735026918962576450914878050196;
+    
+    public const double Log2E = 1.44269504088896326230;
+    public const double Log10E = 0.4342948819032518274635;
+    public const double Ln2 = 0.6931471805599453094172321215;
+}
