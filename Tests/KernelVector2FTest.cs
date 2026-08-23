3d87a5fe6f583d07a3247857b4095a627cd4b844
Zq-est <ZqMyr206088@outloook.com>
2026-08-23 20:48:50 +0800

    test(MathLib): add initial Vector2F unit tests and configure parallel execution
    - Introduce KernelVector2FTest with default constructor test skeleton
    - Configure MSTest method-level parallelism via MSTestSettings.cs
    - Set up testing foundation for 2D vector operations in Harmony Engine

---
+++ Tests/KernelVector2FTest.cs
@@ -0,0 +1,14 @@
+﻿using Kernel.MathLib;
+
+namespace Tests;
+
+[TestClass]
+public class KernelVector2FTest
+{
+    [TestMethod]
+    public void KernelVector2FConstructorDefaultTest()
+    {
+        
+    }
+    
+}
